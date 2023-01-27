b433ab9f4e2be94b022c2fad50bed6ee1458a708
Vince Buffalo <vsbuffalo@gmail.com>
2023-01-27 13:12:23 -0800

    added yanky for pasting

--- .config/nvim/lua/vinceb/packer.lua
+++ .config/nvim/lua/vinceb/packer.lua
@@ -51,5 +51,19 @@ return require('packer').startup(function(use)
 
     use 'lervag/vimtex'
 
-end)
+    use("gbprod/yanky.nvim") require("yanky").setup({
+        ring = {
+            history_length = 100,
+            storage = "shada",
+            sync_with_numbered_registers = true,
+            cancel_event = "update",
 
+        },
+        highlight = {
+            on_put = true,
+            on_yank = true,
+            timer = 500,
+        },
+    })
+
+end)
--- .config/nvim/lua/vinceb/remap.lua
+++ .config/nvim/lua/vinceb/remap.lua
@@ -29,3 +29,12 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- packer sync
 vim.keymap.set("n", "<leader>ss", vim.cmd.PackerSync)
 
+-- navigation hacks to move around split buffers more effectively
+vim.keymap.set("n", "<A-h>", "<C-w>h")
+vim.keymap.set("n", "<A-j>", "<C-w>j")
+vim.keymap.set("n", "<A-k>", "<C-w>k")
+vim.keymap.set("n", "<A-l>", "<C-w>l")
+
+-- emacs-like movement in insert mode
+vim.keymap.set("i", "<M-b>", "<Esc>b")
+vim.keymap.set("i", "<M-f>", "<Esc>f")
